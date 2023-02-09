a076a13b0c4c1f71a308d597c373216ac8516c70
Dennis Paixao <dennispaixaoads@gmail.com>
2023-02-09 13:10:28 -0300

    rotas

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,11 +1,30 @@
-    const express= require('express');
+const express= require('express');
 
 const router = express.Router();
 
-router.get('checklists', (req, res)=>{
+router.get('/', (req, res)=>{
     console.log("olá");
-    req.send();
+    res.send();
 
-}
+})
 
-)
+router.post('/', (req,res)=>{
+    console.log(req.body);
+    res.status(200).send(req.body);
+})
+
+router.get('/:id', (req,res)=>{
+    console.log(req.params.id);
+    res.send( req.params);
+})
+
+router.put('/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send( 'put id' +req.params.id);
+})
+
+router.delete('/:id',(req,res)=>{
+    console.log(req.params.id);
+    res.send( 'delete id' +req.params.id);
+})
+module.exports= router;
